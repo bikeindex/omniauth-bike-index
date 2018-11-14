9dda1e0123595bf78ce08973d84ff9a56da8613b
Seth Herr <seth.william.herr@gmail.com>
2018-11-14 12:23:44 -0800

    Add "partner" authorize parameter (#3)

--- lib/omniauth/strategies/bike_index.rb
+++ lib/omniauth/strategies/bike_index.rb
@@ -1,43 +1,43 @@
-require 'omniauth-oauth2'
+require "omniauth-oauth2"
 
 module OmniAuth
   module Strategies
     class BikeIndex < OmniAuth::Strategies::OAuth2
       option :name, :bike_index
-      DEFAULT_SCOPE = 'public'
-      option :client_options, site: 'https://bikeindex.org', authorize_url: '/oauth/authorize'
+      DEFAULT_SCOPE = "public"
+      option :client_options, site: "https://bikeindex.org", authorize_url: "/oauth/authorize"
 
-      uid { raw_info['id'] }
+      uid { raw_info["id"] }
 
       info do
         prune!(
-          'nickname'    => raw_info['user']['username'],
-          'bike_ids'    => raw_info['bike_ids'],
-          'email'       => raw_info['user']['email'],
-          'name'        => raw_info['user']['name'],
-          'twitter'     => raw_info['user']['twitter'],
-          'image'       => raw_info['user']['image'],
+          "nickname"    => raw_info["user"]["username"],
+          "bike_ids"    => raw_info["bike_ids"],
+          "email"       => raw_info["user"]["email"],
+          "name"        => raw_info["user"]["name"],
+          "twitter"     => raw_info["user"]["twitter"],
+          "image"       => raw_info["user"]["image"],
         )
       end
 
       extra do
         hash = {}
-        hash['raw_info'] = raw_info unless skip_info?
+        hash["raw_info"] = raw_info unless skip_info?
         prune! hash
       end
 
       def raw_info
-        @raw_info ||= access_token.get('/api/v3/me').parsed || {}
+        @raw_info ||= access_token.get("/api/v3/me").parsed || {}
       end
 
       def request_phase
-        options[:authorize_params] = { scope: (options['scope'] || DEFAULT_SCOPE) }
+        options[:authorize_params] = { scope: (options["scope"] || DEFAULT_SCOPE), partner: options["partner"] }
         super
       end
 
       # https://github.com/omniauth/omniauth-oauth2/issues/81
       def callback_url
-         full_host + script_name + callback_path
+        full_host + script_name + callback_path
       end
 
       private
